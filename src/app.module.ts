c4a704404f8a3bdc816759fc0a1ac838cd1db1db
Darius Paraschiv <darius.paraschiv@msg.group>
2024-07-17 11:57:48 +0300

    Chapter 4: succesfully integrated TypeOrm with PostogPostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,25 @@ import { HealthController } from './health.controller';
 import { CustomersModule } from './customers/customers.module';
 import { ProductsModule } from './products/products.module';
 import { SharedModule } from './shared/shared.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CustomersModule, OrdersModule, ProductsModule, SharedModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'msgcsuser',
+      password: 'msgcspass',
+      database: 'msgcsdb',
+      entities: [],
+      synchronize: true,
+    }),
+    CustomersModule,
+    OrdersModule,
+    ProductsModule,
+    SharedModule,
+  ],
   controllers: [HealthController],
   providers: [],
 })
